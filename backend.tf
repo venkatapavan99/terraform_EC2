89f8b7812db7bdfe4c0a36008aa5de03c25f6aa4
Mareedu Venkata Pavan <84392859+venkatapavan99@users.noreply.github.com>
2026-02-28 12:25:38 +0530

    Change Terraform state file key for S3 backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "pavan-jenkins-s3-2026-02026"
-    key            = "dev/terraform.tfstate"
+    key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock"
     encrypt        = true
