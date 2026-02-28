dc140fd6754ebbbfaad9fb257487d28ea849bfec
root <root@ip-172-31-26-51.ec2.internal>
2026-02-28 14:09:19 +0000

    Added main.tf file

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ resource "aws_instance" "ec2" {
   Name = "Test" 
 }
 }
+resource "aws_s3_bucket" "best1" {
+   bucket = "vinithaprakbharhclhyd"
+ }
+resource "aws_s3_bucket_versioning" "version" {
+  bucket = aws_s3_bucket1.id 
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
